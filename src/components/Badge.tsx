3665902fc154507db935544919617f46f85c34ff
Tristan D. <tristantheb@users.noreply.github.com>
2026-04-14 03:13:50 +0200

    chore(componens): typed return value for JSX

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,4 +1,11 @@
-const Badge = ({ text, color }) => (
+import { JSX } from "react"
+
+type BadgeProps = {
+  text: string
+  color: string
+}
+
+const Badge = ({ text, color }: BadgeProps): JSX.Element => (
   <div className={'badge'} style={{ backgroundColor: color }}>
     <span className={'badge__label'}>{text}</span>
   </div>
--- src/components/OptionnalInline.tsx
+++ src/components/OptionnalInline.tsx
@@ -1,4 +1,6 @@
-const OptionnalInline = () => (
+import { JSX } from "react"
+
+const OptionnalInline = (): JSX.Element => (
   <div className={'badge-inline'}>
     <span className={'badge-inline__label'}>Facultatif</span>
   </div>
