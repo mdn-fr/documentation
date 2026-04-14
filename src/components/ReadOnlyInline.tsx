9467f580b3876548b2f51d5246da031ac8f6eddf
Tristan D. <tristantheb@users.noreply.github.com>
2026-04-14 03:18:42 +0200

    chore(componens): typed return value for JSX (#11)

--- src/components/ReadOnlyInline.tsx
+++ src/components/ReadOnlyInline.tsx
@@ -1,4 +1,6 @@
-const ReadOnlyInline = () => (
+import { JSX } from "react"
+
+const ReadOnlyInline = (): JSX.Element => (
   <div className={'badge-inline'}>
     <span className={'badge-inline__label'}>Lecture seule</span>
   </div>
